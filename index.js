2a5add527889240ebae1d147ab4728c2bb9c4a93
Pavan Kumar <108340070+PavanP16@users.noreply.github.com>
2022-09-01 17:58:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var n1= prompt("Enter a number in between 1-6");
-var image1 = "images/dice"+ n1 +".png";
+var image1 = "./images/dice"+ n1 +".png";
 document.querySelector(".img1").setAttribute("src" , image1);
 
 var n2= Math.floor(Math.random() * 6) + 1;
-var image2 = "images/dice"+ n2 +".png";
+var image2 = "./images/dice"+ n2 +".png";
 document.querySelector(".img2").setAttribute("src" , image2);
 
 if(n1 > n2)
